e14e2b6ca873b7aaa5cc9480e18e33a98fc871a7
miyuta <yuemiyuta@gmail.com>
2021-07-20 23:07:12 +0900

    url

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import auth from '@/middleware/auth'
 
 export default ({ $axios, redirect, store }) => {
   // $axios.setBaseURL(process.env.API_BASE_URL)
-  $axios.setBaseURL('https://34.84.53.56')
+  $axios.setBaseURL('http://34.84.53.56')
 
   $axios.onRequest((config) => {
     const accessToken = auth.getAccessToken()
